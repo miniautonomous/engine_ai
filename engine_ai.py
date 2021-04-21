db1f4badef966060e3816213f4877403ee7865cd
jesbot <jose.e.solomon@gmail.com>
2021-04-21 09:59:09 -0700

    Temp condition where full autonomous is still manual throttle.

--- engine_ai.py
+++ engine_ai.py
@@ -350,12 +350,17 @@ class EngineApp(App):
             self.root.powerCtrls.ai_steering.bgnColor = [0, 1, 0, 1]
             self.root.powerCtrls.ai_full.bgnColor = [0.7, 0.7, 0.7, 1]
         else:
-            # Full Autonomous!!
-            rescaled_throttle = self.data_utils.map_function(drive_inference[1],
-                                                             [0, 100,
-                                                              self.ui.throttle_min,
-                                                              self.ui.throttle_max])
-            self.arduino_board.Servos.write(THROTTLE_SERVO, rescaled_throttle)
+            # Full Autonomous!! Not yet, need to fix the PWM issue
+            throttle_output = self.arduino_board.throttleIn()
+            rescaled_throttle = self.data_utils.chop_value(throttle_output,
+                                                           self.ui.throttle_min,
+                                                           self.ui.throttle_max)
+            self.arduino_board.Servos.write(THROTTLE_SERVO, throttle_output)
+            # rescaled_throttle = self.data_utils.map_function(drive_inference[1],
+            #                                                  [0, 100,
+            #                                                   self.ui.throttle_min,
+            #                                                   self.ui.throttle_max])
+            # self.arduino_board.Servos.write(THROTTLE_SERVO, rescaled_throttle)
 
             # Update UI
             self.root.powerCtrls.manual.bgnColor = [0.7, 0.7, 0.7, 1]
